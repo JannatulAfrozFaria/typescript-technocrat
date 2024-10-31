049a80786fa87950bbee55a546ebdee634081598
Afroz Faria <afroz_faria03@yahoo.com>
2024-11-01 05:40:17 +0600

    Object created

---
+++ module1/source/1.5.ts
@@ -0,0 +1,10 @@
+//REFERENCE type-----Object
+const user :{
+    firstName:string;
+    middleName: string,
+    lastName:string
+} = {
+    firstName: 'Mezbaul',
+    middleName: 'Abedin',
+    lastName: 'Persian'
+}
