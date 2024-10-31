57f486690dce16a89f098a05844cc5a20805546b
Afroz Faria <afroz_faria03@yahoo.com>
2024-11-01 05:49:27 +0600

    access modifier explored

--- module1/source/1.5.ts
+++ module1/source/1.5.ts
@@ -1,10 +1,16 @@
 //REFERENCE type-----Object
 const user :{
+    readonly company:'PH'; //type---->literal types
     firstName:string;
-    middleName: string,
-    lastName:string
+    middleName?: string; //optional type
+    lastName:string;
+    isMarried:boolean;
 } = {
+    company:'PH',
     firstName: 'Mezbaul',
     middleName: 'Abedin',
-    lastName: 'Persian'
-}
+    lastName: 'Persian',
+    isMarried: true
+};
+
+user.company='p'
