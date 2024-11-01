addaf1a07cce35ebefe0566adbd74d492088688c
Afroz Faria <afroz_faria03@yahoo.com>
2024-11-01 06:23:28 +0600

    destructuring added

--- module1/source/1.8.ts
+++ module1/source/1.8.ts
@@ -1,1 +1,20 @@
-//destructuring
+{
+    //destructuring
+    const user = {
+        id:345,
+        name:{
+            firstName: 'Mezbaul',
+            middleName: 'Abedin',
+            lastName: 'Persian',
+        },
+        contactNo: "01XXXXXXXX",
+        address: 'Uganda',
+    };
+
+    //destructuring the properties
+    const{
+    contactNo,
+    name:{middleName}
+    } = user;
+}
+
