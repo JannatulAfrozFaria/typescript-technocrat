c02921e8f2bee8bac929691906f0edac74f7a391
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2024-12-06 02:11:53 +0600

    student 2 defined

--- module1/source/1.9.ts
+++ module1/source/1.9.ts
@@ -12,5 +12,17 @@
         gender: 'male',
         contactNo: 'o17sssssss',
         address: 'ctg',
+    };
+
+    const student2:{
+        name: string;
+        age: number;
+        gender: string;
+        address: string;
+    } = {
+        name: 'MIR',
+        age: 40,
+        gender: 'male',
+        address: 'dhaka'
     }
 }
