6b738c64f46ad613477532a8e6aacf6be5748e8c
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2024-12-06 02:37:51 +0600

    ternary operator example created

---
+++ module1/source/1.11.ts
@@ -0,0 +1,11 @@
+{
+    // 
+    //TERNARY OPERATOR-----OPTIONAL CHAINING-----NULLISH COALESCING
+    const age: number = 15;
+    if(age >= 18){
+        console.log('adult');
+    }else{
+        console.log('not adult')
+    }
+    // 
+}
