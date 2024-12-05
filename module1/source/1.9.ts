044d8a4932f925ae8f11789d7d2cca25252d823e
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2024-12-06 02:09:42 +0600

    address defined

---
+++ module1/source/1.9.ts
@@ -0,0 +1,16 @@
+{
+    //TYPE ALIAS-------
+    const student1: {
+        name: string;
+        age: number;
+        gender: string;
+        contactNo: string;
+        address: string;
+    } = {
+        name: 'Mezba',
+        age: 50,
+        gender: 'male',
+        contactNo: 'o17sssssss',
+        address: 'ctg',
+    }
+}
