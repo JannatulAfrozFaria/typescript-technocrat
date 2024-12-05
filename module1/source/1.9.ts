10dcb01cb05b32739b7451f3bb99e1d6afd3cbdc
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2024-12-06 02:19:39 +0600

    Example of function added

--- module1/source/1.9.ts
+++ module1/source/1.9.ts
@@ -35,4 +35,7 @@
     //EXAMPLE---3
     type IsAdmin = boolean
     const isAdmin : IsAdmin = true
+    //EXAMPLE---4
+    type AddFunction = (num1: number,num2: number) => number;
+    const add: AddFunction = (num1,num2) => num1 + num2;
 }
