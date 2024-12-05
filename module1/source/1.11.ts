a3b057e3f5921b3671e650927fb688513ce1229f
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2024-12-06 03:13:46 +0600

    nullish qualescing operator explained

--- module1/source/1.11.ts
+++ module1/source/1.11.ts
@@ -1,11 +1,42 @@
 {
     // 
     //TERNARY OPERATOR-----OPTIONAL CHAINING-----NULLISH COALESCING
-    const age: number = 15;
+    const age: number = 18;
     if(age >= 18){
         console.log('adult');
     }else{
         console.log('not adult')
     }
-    // 
+    //using ternary operator---
+    const isAdult = age >= 18 ? 'adult' : 'not adult';
+    console.log({isAdult});
+    //nullish coalescing operator
+    //nulll/ undefined----> decision making
+    const isAutheticated = null;
+    const result1 = isAutheticated ?? 'Guest';
+    const result2 = isAutheticated ? isAutheticated :'Guest';
+
+    console.log({result1}, {result2});
+
+    //COALESCING
+    type User = {
+        name: string;
+        address:{
+            city: string;
+            road: string;
+            presentAddress: string;
+            permanentAddress?:string
+        }
+    }
+    const user: User ={
+        name: 'p',
+        address:{
+            city: 'ctg',
+            road: 'Awesome road',
+            presentAddress: 'ctg town'
+    }
+    }
+    const permanentAddress = user?.address?.permanentAddress ?? 'No permanent Address';
+    console.log({permanentAddress});
+   // 
 }
