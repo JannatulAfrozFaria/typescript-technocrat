5aa8cb46047b27864340df023cfab7d66a1ce8c4
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2024-12-06 03:24:43 +0600

    unknown type explanation started

--- module1/source/1.12.ts
+++ module1/source/1.12.ts
@@ -1,7 +1,7 @@
 {
     //
     //nullable type
-    const searchName = (value:string)=>{
+    const searchName = (value:string | null)=>{
         if(value){
             console.log('Searching');
         }else{
@@ -9,5 +9,22 @@
         }
     };
     searchName("Persian")
+
+    //unknown type
+    const getSpeedInMeterPerSecond = (value:unknown)=>{
+        if(typeof value === 'number'){
+            const convertedSpeed = (value*1000)/3600;
+            console.log(`The speed is ${convertedSpeed} ms^-1`)
+        }
+        if(typeof value === 'string'){
+            const [result,unit] = value.split(' ')
+            const convertedSpeed = (parseFloat(result)*1000)/3600;
+            console.log(`The speed is ${convertedSpeed} ms^-1`);
+        }
+        else {
+            console.log('wrong input');
+        }
+    }
+    getSpeedInMeterPerSecond(1000);
     //
 }
