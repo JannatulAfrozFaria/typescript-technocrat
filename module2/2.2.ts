cd759be8f9e9b651af567f07324c0853e92046cf
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2025-01-24 11:00:24 +0600

    & extend use shown

--- module2/2.2.ts
+++ module2/2.2.ts
@@ -8,10 +8,7 @@
     type roll = number;
 
     //in case of Object, we can use interface
-    interface User2{
-        name:string;
-        age:number;
-    }
+    
     const user1: User1 = {
         name: 'Faria',
         age: 100,
@@ -19,5 +16,27 @@
     // 
 
     //intersect
+    //to extend type, we have to use & 
     type UserWithRoll1 = User1 & {role : string}
+
+    const user2:UserWithRoll1 = {
+        name: 'Parsa',
+        age: 90,
+        role: 'student' //if role not added, it will show error
+    }
+
+    //with interface
+    interface User2{
+        name:string;
+        age:number;
+    }
+    //to extend interface, we have to use extend key word
+    interface UserWithRoll2 extends User2 {
+        role: string;
+    }
+    const user3:UserWithRoll2 = {
+        name: 'Parsa',
+        age: 90,
+        role: 'student' //if role not added, it will show error
+    }
 }
