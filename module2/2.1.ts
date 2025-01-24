1d15ab4c54ef5079d1c25a378a85739615ea5cb7
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2025-01-24 10:41:53 +0600

    type assertion added

--- module2/2.1.ts
+++ module2/2.1.ts
@@ -2,7 +2,18 @@
     //
     //type assertion
     let anything : any;
-    anything = "Next Level Web Development"
-    anything = true
+    anything = "Next Level Web Development";
+    anything = true;
+    (anything as string).charAt(0)
+
+    const kgToGram = (value:string | number ) =>{
+        if(typeof value === 'string'){
+            const convertedValue = parseFloat(value)*1000
+            return `the converted value is  ${convertedValue} `;
+        }
+        if(typeof value === "number"){
+            return value*1000
+        }
+    }
     //
 }
