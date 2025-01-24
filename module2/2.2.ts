cff69462ce4f176f1da5b3c44e727eecbace28b8
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2025-01-24 10:52:42 +0600

    type necessity added

--- module2/2.2.ts
+++ module2/2.2.ts
@@ -4,6 +4,10 @@
         name: string;
         age: number;
     }
+    //type alias is needed for prmitive type
+    type roll = number;
+
+    //in case of Object, we can use interface
     interface User2{
         name:string;
         age:number;
