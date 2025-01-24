632490cfa9875f5d3319c90585e4edf11b5d6387
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2025-01-24 10:50:26 +0600

    inteefcae added

---
+++ module2/2.2.ts
@@ -0,0 +1,16 @@
+{
+    // 
+    type User1 = {
+        name: string;
+        age: number;
+    }
+    interface User2{
+        name:string;
+        age:number;
+    }
+    const user1: User1 = {
+        name: 'Faria',
+        age: 100,
+    }
+    // 
+}
