6c7718bf528238d60a07ffb1934de246c51d4905
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2025-01-31 01:49:46 +0600

    added

---
+++ module2/data.table.pagination.ts
@@ -0,0 +1,14 @@
+// const table = useReactTable({
+	// 	data: filteredData,
+	// 	columns,
+	// 	getCoreRowModel: getCoreRowModel(),
+	// 	getPaginationRowModel: getPaginationRowModel(),
+	// 	getSortedRowModel: getSortedRowModel(),
+	// 	onColumnFiltersChange: setColumnFilters,
+	// 	getFilteredRowModel: getFilteredRowModel(),
+	// 	state: {
+	// 		columnFilters,
+	// 	},
+	// });
+
+	// const paginatedData = table.getRowModel().rows.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage);
