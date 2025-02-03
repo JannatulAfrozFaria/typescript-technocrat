db69a7625a87a76cb41ade34a2c9e8afbfecc74b
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2025-02-04 01:05:01 +0600

    T added to param

--- module2/2.5.ts
+++ module2/2.5.ts
@@ -3,7 +3,7 @@
     const createArray = (param:string) : string[] =>{
         return [param]
     }
-    const createArrayWithGeneric = (param:string) : string[] =>{
+    const createArrayWithGeneric = <T> (param:T) : T[] =>{
         return [param]
     }
     const res1 = createArray('Bangladesh')
