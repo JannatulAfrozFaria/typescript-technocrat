ced1cf27973cdde24c33ebb77a2daaf2f6fbe80e
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2025-02-04 01:25:14 +0600

    student2 added

--- module2/2.5.ts
+++ module2/2.5.ts
@@ -27,6 +27,7 @@
             course
         }
     }
-
+    const student1 = addCourseToStudent({name:'M0',email:'gmail', devType:'Next'})
+    const student2 =addCourseToStudent({name:'Y',email:'yahoo',devType:'basic'})
     //
 }
