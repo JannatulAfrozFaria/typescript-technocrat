56f32c17cabc72f04861a349ffa4038d19511337
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2025-02-04 01:10:41 +0600

    object added to generic type

--- module2/2.5.ts
+++ module2/2.5.ts
@@ -9,6 +9,6 @@
     const res1 = createArray('Bangladesh')
     const resGeneric = createArrayWithGeneric<boolean>(true);
     const resGenericString = createArrayWithGeneric<string>('Bangladesh');
-    const resGenericObject = createArrayWithGeneric<string>({id:222,name:'Mr'});
+    const resGenericObject = createArrayWithGeneric<{id:number, name: string}>({id:222,name:'Mr'});
     //
 }
