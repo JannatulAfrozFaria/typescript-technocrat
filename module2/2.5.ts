5f4d395af2a8dca9ecc2578b8f5beb94cc86ca03
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2025-02-04 01:06:44 +0600

    function called with string

--- module2/2.5.ts
+++ module2/2.5.ts
@@ -8,5 +8,6 @@
     }
     const res1 = createArray('Bangladesh')
     const resGeneric = createArrayWithGeneric<boolean>(true);
+    const resGeneric1 = createArrayWithGeneric<string>('Bangladesh');
     //
 }
