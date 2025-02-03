7e20f2fa37ac17bfa1ffd7d3463b4b50044db5c8
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2025-02-04 01:28:19 +0600

    data copied

--- module2/2.6.ts
+++ module2/2.6.ts
@@ -1,4 +1,13 @@
 {
-    //
+    //constraint
+    const addCourseToStudent =<T> (student: T) =>{
+        const course = 'Next Level Web Development'
+        return{
+            ...student,
+            course
+        }
+    }
+    const student1 = addCourseToStudent({name:'M0',email:'gmail', devType:'Next'})
+    const student2 =addCourseToStudent({name:'Y',email:'yahoo',devType:'basic',hasWatch:'Apple'})
     //
 }
