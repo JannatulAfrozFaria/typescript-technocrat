3c0511ffe28f23e7a6d206095b65119108015958
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2025-02-04 01:33:11 +0600

    add

--- module2/2.6.ts
+++ module2/2.6.ts
@@ -1,14 +1,23 @@
 {
     //constraint
-    const addCourseToStudent =<T> (student: T) =>{
+    const addCourseToStudent =<T extends {
+        id: number;
+        name: string;
+        email: string;
+    }> (student: T) =>{
         const course = 'Next Level Web Development'
         return{
             ...student,
             course
         }
     }
-    const student1 = addCourseToStudent({id:21,name:'M0',email:'gmail', devType:'Next'})
-    const student2 =addCourseToStudent({id:22,name:'Y',email:'yahoo',devType:'basic',hasWatch:'Apple'})
-    const student3 =addCourseToStudent({id:22,name:'Y',email:'yahoo',devType:'basic',hasWatch:'Apple'})
+    const student1 = addCourseToStudent<{
+        id: number;
+        name: string;
+        email: string;
+        devType: string;
+    }> ({id:21,name:'M0',email:'gmail', devType:'Next'})
+    const student2 = addCourseToStudent({id:22,name:'Y',email:'yahoo',devType:'basic',hasWatch:'Apple'})
+    const student3 = addCourseToStudent({id:22,name:'Y',email:'yahoo',devType:'basic',hasWatch:'Apple'})
     //
 }
