2354aa9b177668780f2aa294472967cb9aa18ef9
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2025-02-04 01:29:44 +0600

    student3 declared

--- module2/2.6.ts
+++ module2/2.6.ts
@@ -7,7 +7,8 @@
             course
         }
     }
-    const student1 = addCourseToStudent({name:'M0',email:'gmail', devType:'Next'})
-    const student2 =addCourseToStudent({name:'Y',email:'yahoo',devType:'basic',hasWatch:'Apple'})
+    const student1 = addCourseToStudent({id:21,name:'M0',email:'gmail', devType:'Next'})
+    const student2 =addCourseToStudent({id:22,name:'Y',email:'yahoo',devType:'basic',hasWatch:'Apple'})
+    const student3 =addCourseToStudent({id:22,name:'Y',email:'yahoo',devType:'basic',hasWatch:'Apple'})
     //
 }
