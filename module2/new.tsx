1032b0e49bffa1e4b49c4f79c4b68bbf55f3013c
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2025-02-08 00:23:56 +0600

    1 removed

--- module2/new.tsx
+++ module2/new.tsx
@@ -307,7 +307,7 @@ export default function SalesOverviewData() {
 													}}
 												>
 													Next
-												</Button>1
+												</Button>
 											</div>
 										</div>
 									) : (
