f5d352619735e27c05893cae2b6a04b94742ad4b
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2025-02-11 00:25:39 +0600

    user added

--- module2/2.7.ts
+++ module2/2.7.ts
@@ -8,5 +8,11 @@
     type Owner = "bike" | "car" | "ship" ; //manually
     type Owner2 = keyof Vehicle
     const person1:Owner2 = ""
+    const user = {
+        name: 'Mr. persi',
+        age: 27,
+        address: 'ctg'
+    }
+    user['name']
     //
 }
