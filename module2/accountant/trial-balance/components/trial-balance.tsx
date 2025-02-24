7f56641a9330940bcb78b1e76fc1dac4eab3f142
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2025-02-24 15:38:36 +0600

    tn added

--- module2/accountant/trial-balance/components/trial-balance.tsx
+++ module2/accountant/trial-balance/components/trial-balance.tsx
@@ -4,10 +4,7 @@ import { trialBalanceTableColumns } from './trial-balance-table-columns';
 import type { JournalEntry } from '@/types/types';
 import { useDebounce } from 'use-debounce';
 import Loader from '@/components/loader';
-import Pagination from '@/components/pagination';
 import HeaderWithButtons from '@/components/header-with-buttons';
-import EmptyPage from '@/components/empty-page';
-import SalesHomeImage from '@/components/icon-components/sales-home-image';
 import TrialBalanceTopBar from './trial-balance-top-bar';
 import TrialBalanceDataTable from './trial-balance-data-table';
 
@@ -31,6 +28,136 @@ const data: JournalEntry[] = [
 		debit: '-',
 		credit: 300,
 		amount: 300,
+		type: 'Assets',
+	},
+	{
+		id: '3',
+		date: new Date('03.02.2025'),
+		description: 'Office rent payment',
+		account: 'Rent expense',
+		debit: 800,
+		credit: '-',
+		amount: 800,
+		type: 'Expenses',
+	},
+	{
+		id: '4',
+		date: new Date('03.02.2025'),
+		description: 'Purchased Wireless Mouse',
+		account: 'cash',
+		debit: '-',
+		credit: 500,
+		amount: 500,
+		type: 'Assets',
+	},
+	{
+		id: '5',
+		date: new Date('03.02.2025'),
+		description: 'Equipment purchase',
+		account: 'Machinery & equipment',
+		debit: 320,
+		credit: '-',
+		amount: 320,
+		type: 'Assets',
+	},
+	{
+		id: '6',
+		date: new Date('03.02.2025'),
+		description: 'Purchased Scientific Calculator',
+		account: 'Accounts payable',
+		debit: '-',
+		credit: 700,
+		amount: 700,
+		type: 'Liabilities',
+	},
+	{
+		id: '7',
+		date: new Date('03.02.2025'),
+		description: 'Loan repayment',
+		account: 'Loan payable',
+		debit: 250,
+		credit: '-',
+		amount: 250,
+		type: 'Liabilities',
+	},
+	{
+		id: '8',
+		date: new Date('03.02.2025'),
+		description: 'Loan repayment',
+		account: 'cash',
+		debit: '-',
+		credit: 400,
+		amount: 400,
+		type: 'Assets',
+	},
+	{
+		id: '9',
+		date: new Date('03.02.2025'),
+		description: 'Employee salary payment',
+		account: 'Salaries expense',
+		debit: 100,
+		credit: '-',
+		amount: 100,
+		type: 'Expenses',
+	},
+	{
+		id: '10',
+		date: new Date('03.02.2025'),
+		description: 'Purchased Wall Clock',
+		account: 'cash',
+		debit: '-',
+		credit: 460,
+		amount: 460,
+		type: 'Assets',
+	},
+	{
+		id: '11',
+		date: new Date('03.02.2025'),
+		description: 'Purchased Clipboards (Pack of 5)',
+		account: 'cash',
+		debit: 25,
+		credit: '-',
+		amount: 25,
+		type: 'Assets',
+	},
+	{
+		id: '12',
+		date: new Date('03.02.2025'),
+		description: 'Sales revenue from product',
+		account: 'Sales revenue',
+		debit: '-',
+		credit: 600,
+		amount: 600,
+		type: 'Revenues',
+	},
+	{
+		id: '13',
+		date: new Date('03.02.2025'),
+		description: 'Employee salary payment',
+		account: 'cash',
+		debit: 90,
+		credit: '-',
+		amount: 90,
+		type: 'Assets',
+	},
+	{
+		id: '14',
+		date: new Date('03.02.2025'),
+		description: 'Sales revenue from product',
+		account: 'cash',
+		debit: '-',
+		credit: 250,
+		amount: 250,
+		type: 'Assets',
+	},
+	{
+		id: '15',
+		date: new Date('03.02.2025'),
+		description: 'Purchased LED Desk Lamp',
+		account: 'cash',
+		debit: 45,
+		credit: '-',
+		amount: 45,
 		type: 'Assets',
 	},
 ];
@@ -80,24 +207,24 @@ export default function TrialBalance() {
 		setCurrentPageNumber(0);
 	}, [debouncedSearch, data, filters]);
 
-	const totalPages = Math.ceil(filteredData.length / itemsPerPage);
-	const paginatedData = filteredData.slice(
-		currentPageNumber * itemsPerPage,
-		(currentPageNumber + 1) * itemsPerPage
-	);
+	// const totalPages = Math.ceil(filteredData.length / itemsPerPage);
+	// const paginatedData = filteredData.slice(
+	// 	currentPageNumber * itemsPerPage,
+	// 	(currentPageNumber + 1) * itemsPerPage
+	// );
 
-	const canGoToPreviousPage = () => currentPageNumber > 0;
-	const canGoToNextPage = () => currentPageNumber < totalPages - 1;
+	// const canGoToPreviousPage = () => currentPageNumber > 0;
+	// const canGoToNextPage = () => currentPageNumber < totalPages - 1;
 
-	const handlePageChange = (direction: 'next' | 'previous') => {
-		startTransition(() => {
-			if (direction === 'next' && canGoToNextPage()) {
-				setCurrentPageNumber((prev) => prev + 1);
-			} else if (direction === 'previous' && canGoToPreviousPage()) {
-				setCurrentPageNumber((prev) => prev - 1);
-			}
-		});
-	};
+	// const handlePageChange = (direction: 'next' | 'previous') => {
+	// 	startTransition(() => {
+	// 		if (direction === 'next' && canGoToNextPage()) {
+	// 			setCurrentPageNumber((prev) => prev + 1);
+	// 		} else if (direction === 'previous' && canGoToPreviousPage()) {
+	// 			setCurrentPageNumber((prev) => prev - 1);
+	// 		}
+	// 	});
+	// };
 
 	if (loadingStatus.isLoading || isPending) {
 		return <Loader message={loadingStatus.message} />;
@@ -119,27 +246,28 @@ export default function TrialBalance() {
 							setSearchTerm={setSearchTerm}
 						/>
 						<TrialBalanceDataTable
-							data={paginatedData}
+							data={filteredData}
 							columns={trialBalanceTableColumns}
 						/>
-						<Pagination
+						{/* <Pagination
 							handlePageChange={handlePageChange}
 							isPending={isPending}
 							canGoToPreviousPage={canGoToPreviousPage}
 							canGoToNextPage={canGoToNextPage}
 							currentPageNumber={currentPageNumber}
 							totalPages={totalPages}
-						/>
+						/> */}
 					</div>
 				</div>
 			) : (
-				<EmptyPage
-					heading='No Trial Balance found'
-					subtitle='Record a new trial balance manually'
-					buttonText='+ Create Trial Balance'
-					buttonLink='/en/products/new-product'
-					SvgComponent={SalesHomeImage}
-				/>
+				// <EmptyPage
+				// 	heading='No Trial Balance found'
+				// 	subtitle='Record a new trial balance manually'
+				// 	buttonText='+ Create Trial Balance'
+				// 	buttonLink='/en/products/new-product'
+				// 	SvgComponent={SalesHomeImage}
+				// />
+				''
 			)}
 		</div>
 	);
