5af06dfe9d560eb1f9c3b746c29babc9492811f0
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2025-03-11 11:26:49 +0600

    form replaced

--- module2/contacts/components/contact-form/contact-form.tsx
+++ module2/contacts/components/contact-form/contact-form.tsx
@@ -103,10 +103,140 @@ export default function ContactForm({
 	return (
 		<Form {...form}>
 			<form onSubmit={handleSubmit(onSubmit)}>
+				{/* Radio Group */}
+				<div className='grid grid-cols-2 gap-x-16 mb-10'>
+					{/* CONTACT---TYPE */}
+					<FormField
+						control={form.control}
+						name='type'
+						render={({ field }) => (
+							<FormItem className='space-y-4'>
+								<FormLabel className='text-sm font-medium'>
+									Contact type
+								</FormLabel>
+								<FormControl>
+									<RadioGroup
+										onValueChange={field.onChange}
+										defaultValue={field.value}
+										className='!flex gap-[2.5rem]'
+									>
+										<div className='flex items-center space-x-2'>
+											<RadioGroupItem
+												value='VENDOR'
+												id='vendor'
+												className='h-5 w-5 border border-[#94A3B8] text-white rounded-full data-[state=checked]:border-[4px] data-[state=checked]:border-[#2354E6]  '
+											/>
+											<FormLabel
+												htmlFor='vendor'
+												className='text-sm font-medium'
+											>
+												Vendor
+											</FormLabel>
+										</div>
+										<div className='flex items-center space-x-2'>
+											<RadioGroupItem
+												value='CUSTOMER'
+												id='customer'
+												className='h-5 w-5 border border-[#94A3B8] text-white rounded-full data-[state=checked]:border-[4px] data-[state=checked]:border-[#2354E6]'
+											/>
+											<FormLabel
+												htmlFor='customer'
+												className='text-sm font-medium'
+											>
+												Customer
+											</FormLabel>
+										</div>
+										<div className='flex items-center space-x-2'>
+											<RadioGroupItem
+												value='CUSTOMER_AND_VENDOR'
+												id='both'
+												className='h-5 w-5 border border-[#94A3B8] text-white rounded-full data-[state=checked]:border-[4px] data-[state=checked]:border-[#2354E6]'
+											/>
+											<FormLabel htmlFor='both' className='text-sm font-medium'>
+												Both
+											</FormLabel>
+										</div>
+									</RadioGroup>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					{/* ENTITY---TYPE---- */}
+					<FormField
+						control={form.control}
+						name='entity'
+						render={({ field }) => (
+							<FormItem className='space-y-4'>
+								<FormLabel className='text-sm font-medium'>
+									Entity type
+								</FormLabel>
+								<FormControl>
+									<RadioGroup
+										onValueChange={field.onChange}
+										defaultValue={field.value}
+										className='!flex gap-[2.5rem]'
+									>
+										<div className='flex items-center space-x-2'>
+											<RadioGroupItem
+												value='BUSINESS'
+												id='business'
+												className='h-5 w-5 border border-[#94A3B8] text-white rounded-full data-[state=checked]:border-[4px] data-[state=checked]:border-[#2354E6]'
+											/>
+											<FormLabel
+												htmlFor='business'
+												className='text-sm font-medium'
+											>
+												Business
+											</FormLabel>
+										</div>
+										<div className='flex items-center space-x-2'>
+											<RadioGroupItem
+												value='INDIVIDUAL'
+												id='individual'
+												className='h-5 w-5 border border-[#94A3B8] text-white rounded-full data-[state=checked]:border-[4px] data-[state=checked]:border-[#2354E6]'
+											/>
+											<FormLabel
+												htmlFor='individual'
+												className='text-sm font-medium'
+											>
+												Individual
+											</FormLabel>
+										</div>
+									</RadioGroup>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+				</div>
+
 				{/* Basic Info */}
 				<div>
 					<div className='mb-6'>
+						<h1 className='text-[#0F172A] text-lg font-semibold mb-4'>
+							Basic Info
+						</h1>
+						<div className='grid grid-cols-2 gap-x-6'>
+							<TextField
+								control={control}
+								name='name'
+								label='Name'
+								placeholder='Name'
+							/>
+							<TextField
+								name='email'
+								label='Email'
+								control={control}
+								placeholder='Email'
+							/>
 
+							{/* <TextField
+								name='phoneNumber'
+								label='Phone Number'
+								control={control}
+								placeholder='Phone Number'
+							/> */}
 
 							<FormField
 								control={control}
@@ -116,7 +246,7 @@ export default function ContactForm({
 										{<FormLabel>Phone Number</FormLabel>}
 										<FormControl>
 											<div>
-												<PhoneInput
+												{/* <PhoneInput
 													defaultCountry='es'
 													preferredCountries={['es', 'us']}
 													className='border rounded-md !items-center '
@@ -151,6 +281,41 @@ export default function ContactForm({
 													}}
 													disableDialCodeAndPrefix={!selectedCountry}
 													showDisabledDialCodeAndPrefix={!!selectedCountry}
+												/> */}
+												<PhoneInput
+													defaultCountry='es'
+													preferredCountries={['es', 'us']}
+													className='border rounded-md !items-center'
+													style={{
+														backgroundColor: '#F8FAFC',
+														height: 40,
+														fontFamily: 'axiforma',
+													}}
+													inputClassName='!border-none !bg-transparent !text-sm !font-medium !w-full'
+													countrySelectorStyleProps={{
+														buttonStyle: {
+															backgroundColor: 'transparent',
+															border: 'none',
+															paddingLeft: '0.5rem',
+															paddingRight: '0.5rem',
+														},
+														dropdownStyleProps: {
+															style: {
+																boxShadow: 'none',
+															},
+															className:
+																'!w-[346px] border !px-1 !overflow-y-auto scrollbar-none',
+															listItemClassName: '!py-1 !hover:bg-red-100',
+														},
+													}}
+													placeholder={'Phone Number'}
+													value={field.value as string | undefined}
+													onChange={(phoneNumber, { country }) => {
+														field.onChange(phoneNumber);
+														setSelectedCountry(country.iso2);
+													}}
+													disableDialCodeAndPrefix={!selectedCountry}
+													showDisabledDialCodeAndPrefix={!!selectedCountry}
 												/>
 											</div>
 										</FormControl>
@@ -161,10 +326,101 @@ export default function ContactForm({
 								)}
 							/>
 
-
+							{/* Website Field */}
+							<TextField
+								name='website'
+								label='Website'
+								control={control}
+								placeholder='Website'
+							/>
+							{/* NIF Field */}
+							<TextField
+								name='nif'
+								label='NIF'
+								control={control}
+								placeholder='NIF'
+								type='number'
+							/>
+							{/* VAT Number Field */}
+							<TextField
+								name='vatNumber'
+								label='VAT Number'
+								control={control}
+								placeholder='VAT Number'
+							/>
 						</div>
 					</div>
 
+					{/* Invoice settings */}
+					<div className='mb-6'>
+						<h1 className='text-[#0F172A] text-lg font-semibold mb-4'>
+							Invoice Settings
+						</h1>
+						<div className='grid grid-cols-2 gap-x-6'>
+							<LanguagePicker control={control} />
+							<CurrencyPicker control={control} />
+							<TextField
+								name='discountPercentage'
+								label='Standard discount'
+								control={control}
+								placeholder='0%'
+								type='number'
+							/>
+							{/* Standard Due Date Days */}
+							<TextField
+								name='dueDateInDays'
+								label='Standard due date (days)'
+								control={control}
+								placeholder='14'
+								type='number'
+							/>
+						</div>
+					</div>
+
+					{/* Address */}
+					<div>
+						<h1 className='text-[#0F172A] text-lg font-semibold mb-4'>
+							Address
+						</h1>
+						<div className='grid grid-cols-2 gap-x-6'>
+							<TextField
+								name='address.addressLine1'
+								label='Address Line 1'
+								control={control}
+								placeholder='Address Line 1'
+							/>
+							{/* Postal Code */}
+							<TextField
+								name='address.postalCode'
+								label='Postal Code'
+								control={control}
+								placeholder='Postal Code'
+								type='number'
+							/>
+
+							<TextField
+								name='address.addressLine2'
+								label='Address Line 2'
+								control={control}
+								placeholder='Address Line 2'
+							/>
+							{/* PROVINCE--- */}
+							<TextField
+								name='address.province'
+								label='Province'
+								control={control}
+								placeholder='Province'
+							/>
+							<TextField
+								name='address.city'
+								label='City'
+								control={control}
+								placeholder='City'
+							/>
+							{/* Country Dropdown */}
+							<CountryPicker control={control} name='address.countryId' />
+						</div>
+					</div>
 				</div>
 				{/* BUTTON----- */}
 				<div className='text-left mr-1 mt-5 flex justify-end items-end'>
