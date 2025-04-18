5c4a0256a1fbaeb6d55480a3e419350865c9c3bf
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2025-04-19 02:00:34 +0600

    promise function defined

--- module2/2.8.ts
+++ module2/2.8.ts
@@ -1,4 +1,19 @@
 {
     //PROMISE
-    const createPromise = ()
+    const createPromise = () =>{
+        return new Promise((resolve, reject)=>{
+            const data: string = "something"
+            if(data){
+                resolve(data)
+            }else{
+                reject('failed to load data')
+            }
+        })
+    }
+
+    //callling create promise function
+    const showData = async() =>{
+        const data = await createPromise()
+        console.log(data)
+    }
 }
