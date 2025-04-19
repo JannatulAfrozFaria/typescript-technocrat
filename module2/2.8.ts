543c7efa5da6150b7f651f1a2d904b398b1ff0ed
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2025-04-19 22:23:31 +0600

    add console

--- module2/2.8.ts
+++ module2/2.8.ts
@@ -14,7 +14,7 @@
     //callling create promise function
     const showData = async() =>{
         const data = await createPromise()
-        console.log(data)
+        
     }
     showData();
 }
