691e53d7b48d0f4c81a051b77f3d354008d0178f
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2025-04-22 00:22:26 +0600

    remove

--- module2/2.8.ts
+++ module2/2.8.ts
@@ -11,10 +11,4 @@
         })
     }
 
-    //callling create promise function
-    const showData = async() =>{
-        const data = await createPromise()
-        console.log(data)
-    }
-    showData();
-}
+    
