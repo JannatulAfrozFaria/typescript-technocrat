20d698261bb7d44f94582b1667cf922184d79c16
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2025-04-23 12:41:48 +0600

    promise removed

--- module2/2.8.ts
+++ module2/2.8.ts
@@ -1,15 +1,5 @@
 {
-    //PROMISE
-    const createPromise = ():Promise<string>  =>{
-        return new Promise<string> ((resolve, reject)=>{
-            const data: string = "something"
-            if(data){
-                resolve(data)
-            }else{
-                reject('failed to load data')
-            }
-        })
-    }
+    
 
     //callling create promise function
     const showData = async() =>{
