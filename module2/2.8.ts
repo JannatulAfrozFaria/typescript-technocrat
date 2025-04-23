ed86ccf10b565e2fa1e95513c067d24b26e6f226
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2025-04-23 12:42:18 +0600

    promise created

--- module2/2.8.ts
+++ module2/2.8.ts
@@ -1,5 +1,15 @@
 {
-    
+    //PROMISE
+    const createPromise = ():Promise<string>  =>{
+        return new Promise<string> ((resolve, reject)=>{
+            const data: string = "something"
+            if(data){
+                resolve(data)
+            }else{
+                reject('failed to load data')
+            }
+        })
+    }
 
     //callling create promise function
     const showData = async() =>{
