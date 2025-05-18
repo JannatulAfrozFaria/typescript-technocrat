30034db6c4fe132ce7b32fe16f460d90098c33e6
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2025-05-19 01:04:57 +0600

    remove user

--- module2/2.7.ts
+++ module2/2.7.ts
@@ -12,11 +12,7 @@
         return obj[key];
     }
 
-    const user = {
-        name: 'Mr. persi',
-        age: 27,
-        address: 'ctg'
-    }
+    
     const result = getPropertyValue(user,'name');
     const car = {
         model:"Toyota 100",
